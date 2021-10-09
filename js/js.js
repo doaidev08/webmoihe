b9430a91af43476c081141fd7fff89fa6f043c3f
doaidev08 <89960196+doaidev08@users.noreply.github.com>
2021-10-09 16:48:19 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -3,9 +3,12 @@ $(window).scroll(function() {
    if ($(this).scrollTop()>10)
      {
          $('#top').hide();
+         $('.navbar').addClass( "changeTop");
          $(".scrollTop_button").show();
+
      }else{
          $('#top').show();
+         $('.navbar').removeClass( "changeTop");
          $(".scrollTop_button").hide();
      }
  });
